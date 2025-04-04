365b65fa8cc6ab6a66df36f7984f202ae22737e8
jeancharlesx64 <jean.albuquerque@sptech.school>
2025-04-04 20:33:57 -0300

    [FIX] :: changing domain 'jeancharlesexe' to 'azenithsolutions'

--- src/test/java/com/azenithsolutions/backendapirest/BackendApiRestApplicationTests.java
+++ src/test/java/com/azenithsolutions/backendapirest/BackendApiRestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jeancharlesexe.backendapirest;
+package com.azenithsolutions.backendapirest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
