5313e0c410eba8acce0e31a4e2c2d7b40fa8cea8
jeancharlesx64 <jean.albuquerque@sptech.school>
2025-04-15 14:43:04 -0300

    [FEAT] :: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa yes

---
+++ src/main/java/com/azenithsolutions/backendapirest/v1/controller/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+
+package com.azenithsolutions.backendapirest.v1.controller;
+
+import com.azenithsolutions.backendapirest.v1.dto.ApiErrorResponseDTO;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Hidden
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponseDTO> handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+
+        ApiErrorResponseDTO errorResponse = new ApiErrorResponseDTO(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation Error",
+                errors,
+                path
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
