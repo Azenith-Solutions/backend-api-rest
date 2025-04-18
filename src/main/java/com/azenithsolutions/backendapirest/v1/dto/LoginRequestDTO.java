e6c1fa3bb9e3138231ecdee5a3859393a9df4545
jeancharlesx64 <jean.albuquerque@sptech.school>
2025-04-18 14:41:35 -0300

    [FIX] :: removing validation bean from login DTO

--- src/main/java/com/azenithsolutions/backendapirest/v1/dto/LoginRequestDTO.java
+++ src/main/java/com/azenithsolutions/backendapirest/v1/dto/LoginRequestDTO.java
@@ -12,16 +12,6 @@ import lombok.Setter;
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
 public class LoginRequestDTO {
-    @NotBlank(message = "Email cannot be empty")
-    @Email(message = "Email is not valid")
-    @Pattern(regexp = ".*@.*\\..*", message = "Email must contain @ and domain")
-    @Size(max = 100, message = "Email must be less than 100 characters")
     private String email;
-
-    @NotBlank(message = "Password cannot be empty")
-    @Size(max = 255, message = "Password must be less than 100 characters")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$",
-            message = "Password needs to have: - One capital letter | - One lowercase letter | - One number | - One special character")
-
     private String password;
 }
