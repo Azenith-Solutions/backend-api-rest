b58489aeb816fb0db4cc914c341ffe1d85f2548a
João Victor <joao.melo@sptech.school>
2025-09-05 19:47:38 -0300

    Atualizando endpoints V2 para endpoints semelhantes V1 para manter padrão, mantendo a regra no useCase, sem acoplar o código

--- src/main/java/com/azenithsolutions/backendapirest/v2/infrastructure/web/controller/UserController.java
+++ src/main/java/com/azenithsolutions/backendapirest/v2/infrastructure/web/controller/UserController.java
@@ -3,18 +3,29 @@ package com.azenithsolutions.backendapirest.v2.infrastructure.web.controller;
 import com.azenithsolutions.backendapirest.v2.core.domain.model.user.User;
 import com.azenithsolutions.backendapirest.v2.core.usecase.user.*;
 import com.azenithsolutions.backendapirest.v2.core.usecase.user.command.CreateUserCommand;
+import com.azenithsolutions.backendapirest.v2.infrastructure.web.dto.shared.ApiResponseDTO;
+import com.azenithsolutions.backendapirest.v2.infrastructure.web.dto.user.UserListResponseDTO;
 import com.azenithsolutions.backendapirest.v2.infrastructure.web.dto.user.UserResponseDTO;
+import com.azenithsolutions.backendapirest.v2.infrastructure.web.dto.user.UserUpdateRequestDTO;
+import com.azenithsolutions.backendapirest.v2.infrastructure.web.dto.user.UserUpdateResponseDTO;
 import com.azenithsolutions.backendapirest.v2.infrastructure.web.mappers.UserEntityMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController("userControllerV2")
 @RequestMapping("/v2/user")
@@ -28,12 +39,96 @@ public class UserController {
     private final ListUserUseCase listUserUseCase;
     private final UpdateUserUseCase updateUserUseCase;
 
-    @GetMapping
+    @GetMapping()
     @Operation(summary = "List users", description = "Returns all users (v2 clean architecture)")
-    public ResponseEntity<List<UserResponseDTO>> getAll(){
-        List<User> userList = listUserUseCase.execute();
-        List<UserResponseDTO> userResponseDTOList = userList.stream().map(UserEntityMapper::toResposeDTO).toList();
-        return ResponseEntity.status(200).body(userResponseDTOList);
+    public ResponseEntity<ApiResponseDTO<?>> getUsers(HttpServletRequest request) {
+        try{
+            List<User> users = listUserUseCase.execute();
+
+            List<UserListResponseDTO> userListResponseDTOS = users.stream()
+                    .map(UserEntityMapper::toListResposeDTO).toList();
+
+            return ResponseEntity.ok(
+                    new ApiResponseDTO<>(
+                            LocalDateTime.now(),
+                            HttpStatus.OK.value(),
+                            "Success",
+                            userListResponseDTOS,
+                            request.getRequestURI()
+                    )
+            );
+        }catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new ApiResponseDTO<>(
+                            LocalDateTime.now(),
+                            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                            "Internal Server Error",
+                            List.of("An error occurred while processing the request"),
+                            request.getRequestURI()
+                    )
+            );
+        }
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update user", description = "Returns new user update (v2 clean architecture)")
+    public ResponseEntity<ApiResponseDTO<?>> updateUser(@PathVariable Integer id, @RequestBody UserUpdateRequestDTO body, HttpServletRequest request){
+        try{
+                User user = updateUserUseCase.execute(id, body);
+                UserUpdateResponseDTO responseDTO = UserEntityMapper.toUserUpdateResponseDTO(user);
+
+                return ResponseEntity.status(HttpStatus.OK).body(
+                        new ApiResponseDTO<>(
+                                LocalDateTime.now(),
+                                HttpStatus.OK.value(),
+                                "Success",
+                                List.of(responseDTO),
+                                request.getRequestURI()
+                        )
+                );
+
+        }catch(IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    new ApiResponseDTO<>(
+                            LocalDateTime.now(),
+                            HttpStatus.BAD_REQUEST.value(),
+                            "Bad Request",
+                            e.getMessage(),
+                            request.getRequestURI()
+                    )
+            );
+        }catch(EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new ApiResponseDTO<>(
+                            LocalDateTime.now(),
+                            HttpStatus.NOT_FOUND.value(),
+                            "Not Found",
+                            List.of(e.getMessage()),
+                            request.getRequestURI()
+                    )
+            );
+
+        }catch(EntityExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                    new ApiResponseDTO<>(
+                            LocalDateTime.now(),
+                            HttpStatus.CONFLICT.value(),
+                            "Conflict",
+                            List.of(e.getMessage()),
+                            request.getRequestURI()
+                    )
+            );
+        }catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new ApiResponseDTO<>(
+                            LocalDateTime.now(),
+                            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                            "Internal Server Error",
+                            List.of("An error occurred while processing the request"),
+                            request.getRequestURI()
+                    )
+            );
+        }
     }
 
     @GetMapping("/{id}")
@@ -85,29 +180,6 @@ public class UserController {
             return ResponseEntity.status(204).build();
         }catch (RuntimeException exception){
             return ResponseEntity.status(400).body("Error: %s".formatted(exception.getMessage()));
-        }
-    }
-
-    @PutMapping("/{id}")
-    @Operation(summary = "Update user", description = "Returns new user update (v2 clean architecture)")
-    public ResponseEntity<Map<String, Object>> updateUser(@RequestParam(value = "id")Integer id, @RequestBody CreateUserCommand command){
-        Map<String, Object> response = new HashMap<>();
-
-        try{
-            User user = updateUserUseCase.execute(id, command);
-            UserResponseDTO userResponse = UserEntityMapper.toResposeDTO(user);
-
-            response.put("message", "Usuário atualizado com sucesso!");
-            response.put("user", userResponse);
-
-            return ResponseEntity.status(201).body(response);
-        }catch (IllegalArgumentException exception){
-            response.put("message", "Argumento Inválido: %s".formatted(exception.getMessage()));
-            return ResponseEntity.status(400).body(response);
-        }catch (RuntimeException exception){
-            response.put("message", "Error: %s".formatted(exception.getMessage()));
-            return ResponseEntity.status(400).body(response);
-
         }
     }
 }
