7dd9138cc9a658f8d3e009c475be01d0e9e53ae2
João Victor <joao.melo@sptech.school>
2025-09-07 11:55:34 -0300

    Adicionando interface para token e imageStorage

---
+++ src/main/java/com/azenithsolutions/backendapirest/v2/infrastructure/security/adapter/TokenGatewayAdapter.java
@@ -0,0 +1,64 @@
+package com.azenithsolutions.backendapirest.v2.infrastructure.security.adapter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.azenithsolutions.backendapirest.v2.core.domain.model.user.User;
+import com.azenithsolutions.backendapirest.v2.core.domain.repository.TokenGateway;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenGatewayAdapter implements TokenGateway {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    @Override
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer("backend-api-rest")
+                    .withSubject(user.getEmail().getValue())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Error while creating JWT", e);
+        }
+    }
+
+    @Override
+    public String subjectToken(String token) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("backend-api-rest")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            return null;
+        }
+    }
+
+    @Override
+    public Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    @Override
+    public boolean isTokenValid(String token) {
+        if(token == null || token.isEmpty() || this.subjectToken(token) == null){
+            return false;
+        }
+        return true;
+    }
+}
