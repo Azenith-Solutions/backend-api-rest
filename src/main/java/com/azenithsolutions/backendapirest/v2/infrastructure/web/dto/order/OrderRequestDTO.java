917e629df82fe428c7bffbc2726f7a1438cbeac7
kauansfr <kauan.franca@sptech.school>
2025-12-06 12:40:26 -0300

    feat(order-created-listener): add items field to OrderDTO and OrderRequestDTO for order item management

--- src/main/java/com/azenithsolutions/backendapirest/v2/infrastructure/web/dto/order/OrderRequestDTO.java
+++ src/main/java/com/azenithsolutions/backendapirest/v2/infrastructure/web/dto/order/OrderRequestDTO.java
@@ -1,8 +1,10 @@
 package com.azenithsolutions.backendapirest.v2.infrastructure.web.dto.order;
 
 import com.azenithsolutions.backendapirest.v2.core.domain.model.enums.OrderStatus;
+import com.azenithsolutions.backendapirest.v2.infrastructure.web.dto.item.ItemRequestDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record OrderRequestDTO(
         String codigo,
@@ -12,6 +14,7 @@ public record OrderRequestDTO(
         String valor,
         OrderStatus status,
         String telCelular,
+        List<ItemRequestDTO> items,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
